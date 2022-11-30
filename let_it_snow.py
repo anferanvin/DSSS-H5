888ba8f4b248b24a26df47e66f9a94ee40c34518
anferanvin <andrey.arango@udea.edu.co>
2022-11-30 22:51:46 +0100

    Modified Python file with random colors for each snowflake

--- let_it_snow.py
+++ let_it_snow.py
@@ -11,10 +11,9 @@ def main(speed=0, bg_color="grey"):
     myTurtle.speed(speed)
     # change background color
     turtle_screen.bgcolor(bg_color)
-  
- 
-    """TODO: define different colors here"""
 
+    """TODO: define different colors here"""
+    colors = ['blue', 'green', 'yellow', 'red', 'magenta', 'cyan', 'white', 'purple']
 
     for _ in range(10):
         # define some params
@@ -22,7 +21,7 @@ def main(speed=0, bg_color="grey"):
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
-
+        myTurtle.color(colors[np.random.randint(0, len(colors))])
         # Go to the start position of the snowflake
         myTurtle.penup()
         myTurtle.goto(pos[0], pos[1])
